d40dd4991a591266f7789f08d252e5413c16f7de
Matt Crosier <matt.crosier@gmail.com>
2020-11-12 11:37:23 -0500

    Adding many more listeners/checks.

--- src/main/kotlin/net/crosier/sonar/XQueryChecks.kt
+++ src/main/kotlin/net/crosier/sonar/XQueryChecks.kt
@@ -37,6 +37,39 @@ abstract class AbstractXQueryCheck<L : AbstractXQueryParserListener>(private val
     }
 }
 
+/* ****************** */
+/* CRITICAL CHECKS    */
+/* ****************** */
+class XQueryVersionCheck : AbstractXQueryCheck<XQueryVersionListener>(XQueryVersionListener::class) {
+    override val key = "xquery-version"
+    override val name = "XQuery Version"
+    override val description = "Ensure that you declare the latest XQuery version (1.0-ml/3.0) at the top of each of your scripts (as opposed to declaring an older version - 0.9-ml - or not declaring a version at all). This ensures better compatibility of code after server upgrades and consistent behavior in XQuery processing."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.CRITICAL
+}
+
+class StrongTypingInFunctionDeclarationCheck : AbstractXQueryCheck<StrongTypingInFunctionDeclarationListener>(StrongTypingInFunctionDeclarationListener::class) {
+    override val key = "strong-typing-function-declaration"
+    override val name = "Use Strong Typing in Function Declarations"
+    override val description = "Declare types for function parameters and return types to increase readability and catch potential bugs. Also try to scope the types as narrowly as possible (i.e. use 'element()' instead of 'item()' when returning an element) and include quantifiers on each type."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.CRITICAL
+}
+
+class StrongTypingInModuleVariableCheck : AbstractXQueryCheck<StrongTypingInModuleVariableListener>(StrongTypingInModuleVariableListener::class) {
+    override val key = "strong-typing-module-variable"
+    override val name = "Use Strong Typing when Declaring Module Variable"
+    override val description = "Declare types for declared variables to increase readability and catch potential bugs. Also try to scope the types as narrowly as possible (i.e. use 'element()' instead of 'item()' when the value is an element) and include quantifiers on each type."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.CRITICAL
+}
+
+/* ****************** */
+/* MAJOR CHECKS       */
+/* ****************** */
 class XdmpEvalCheck : AbstractXQueryCheck<XdmpEvalListener>(XdmpEvalListener::class) {
     override val key = "xdmp-eval"
     override val name = "xdmp:eval should be avoided where possible."
@@ -54,3 +87,62 @@ class XdmpValueCheck : AbstractXQueryCheck<XdmpValueListener>(XdmpValueListener:
     override val message = "Avoid using xdmp:value() where possible. Instead use either xdmp:invoke(), xdmp:unpath() or if possible assign functions to variables to dynamically evaluate code logic"
     override val severity = Severity.MINOR
 }
+
+class OperationsInPredicateCheck : AbstractXQueryCheck<OperationsInPredicateListener>(OperationsInPredicateListener::class) {
+    override val key = "operations-in-predicate"
+    override val name = "Avoid Operations in Predicates"
+    override val description = "Instead of calling functions or performing operations in predicates try assigning the results to a variable before the predicate."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.MAJOR
+}
+
+/* ****************** */
+/* MINOR CHECKS       */
+/* ****************** */
+class StrongTypingInFlworCheck : AbstractXQueryCheck<StrongTypingInFlworListener>(StrongTypingInFlworListener::class) {
+    override val key = "strong-typing-in-FLWOR"
+    override val name = "Use Strong Typing FLWOR Expressions"
+    override val description = "Declare types for FLWOR 'let' and 'for' clauses to increase readability and catch potential bugs. Also try to scope the types as narrowly as possible (i.e. use 'element()' instead of 'item()' when the value is an element) and include quantifiers on each type."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.MINOR
+}
+
+class FunctionMappingCheck : AbstractXQueryCheck<FunctionMappingListener>(FunctionMappingListener::class) {
+    override val key = "function-mapping"
+    override val name = "Function Mapping Usage (MarkLogic)"
+    override val description = "Make sure you are intentionally using and/or understand function mapping - otherwise disable it with 'declare option xdmp:mapping \"false\";'. If you wish to use it you should explicitly declare 'declare option xdmp:mapping \"true\";' for readability/maintainability. Please note that this check is Marklogic specific."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.MINOR
+}
+
+class XPathDescendantStepsCheck : AbstractXQueryCheck<XPathDescendantStepsListener>(XPathDescendantStepsListener::class) {
+    override val key = "xpath-descendant-steps"
+    override val name = "Avoid Using '//' in XPath"
+    override val description = "Favor fully-qualified paths in XPath for readability and to avoid potential performance problems."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.MINOR
+}
+
+class XPathTextStepsCheck : AbstractXQueryCheck<XPathTextStepsListener>(XPathTextStepsListener::class) {
+    override val key = "xpath-text-steps"
+    override val name = "Avoid Using text() in XPath"
+    override val description = "Generally avoid using /text() in your XPath in favor of using fn:string() or allowing atomization (through strong typing or default atomization)."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.MINOR
+}
+
+/* TODO: Need to add ability to check entire project functions for effective booleans.
+class EffectiveBooleanCheck : AbstractXQueryCheck<EffectiveBooleanListener>(EffectiveBooleanListener::class) {
+    override val key = "effective-boolean"
+    override val name = "Effective Boolean in Conditional Predicate"
+    override val description = "Unless the value in the conditional is of type xs:boolean it is recommended you use fn:exists(), fn:empty(), or other boolean functions inside of conditional predicates to check values."
+    override val type = RuleType.CODE_SMELL
+    override val message = description
+override val severity = Severity.MINOR
+}
+*/
--- src/main/kotlin/net/crosier/sonar/XQueryParseListeners.kt
+++ src/main/kotlin/net/crosier/sonar/XQueryParseListeners.kt
@@ -1,5 +1,7 @@
 package net.crosier.sonar
 
+import org.antlr.v4.runtime.Token
+import org.antlr.v4.runtime.tree.Trees
 import org.xqdoc.XQueryParser
 import org.xqdoc.XQueryParserBaseListener
 
@@ -10,22 +12,210 @@ abstract class AbstractXQueryParserListener : XQueryParserBaseListener() {
     protected fun addViolation(violation: Violation) = violations.add(violation)
 }
 
+/**
+ * Checks main modules, and sections of main modules for xquery version.
+ * Flags usage of older marklogic based dialect
+ */
+class XQueryVersionListener : AbstractXQueryParserListener() {
+
+    override fun enterModule(ctx: XQueryParser.ModuleContext?) {
+        super.enterModule(ctx)
+
+        if (ctx?.mainModule()?.isNotEmpty() == true) {
+            var hasVersion = false
+            var lineNumber = 1
+            for (child in ctx.children) {
+                when (child.payload) {
+                    is XQueryParser.VersionDeclContext -> {
+                        if ("\"0.9-ml\"" == (child.payload as XQueryParser.VersionDeclContext).version?.text) {
+                            addViolation(Violation(if (lineNumber == 1) 1 else lineNumber + 1))
+                        }
+                        hasVersion = true
+                    }
+                    is XQueryParser.MainModuleContext -> {
+                        if (!hasVersion) {
+                            addViolation(Violation(lineNumber))
+                        }
+                        hasVersion = false
+                    }
+                    is Token -> {
+                        lineNumber = (child.payload as Token).line
+                    }
+                }
+            }
+        } else {  // in library module - including quotes due to parsing rules, and since it is always quoted
+            if (ctx?.versionDecl()?.isEmpty() == true || "\"0.9-ml\"" == ctx?.versionDecl()?.get(0)?.version?.text) {
+                addViolation(Violation(1))
+            }
+        }
+    }
+}
+
+class StrongTypingInFunctionDeclarationListener : AbstractXQueryParserListener() {
+    override fun enterFunctionParam(ctx: XQueryParser.FunctionParamContext?) {
+        super.enterFunctionParam(ctx)
+
+        if (ctx?.typeDeclaration() == null) {
+            addViolation(Violation(ctx!!.DOLLAR().symbol.line))
+        }
+    }
+}
+
+class StrongTypingInModuleVariableListener : AbstractXQueryParserListener() {
+    override fun enterVarDecl(ctx: XQueryParser.VarDeclContext?) {
+        super.enterVarDecl(ctx)
+
+        if (ctx?.typeDeclaration() == null) {
+            addViolation(Violation(ctx!!.KW_VARIABLE().symbol.line))
+        }
+    }
+}
+
 class XdmpEvalListener : AbstractXQueryParserListener() {
     override fun enterQName(ctx: XQueryParser.QNameContext) {
-        val qname = ctx?.FullQName()?.text ?: ""
+        val qname = ctx.FullQName()?.text ?: ""
 
         if (qname.toLowerCase() == "xdmp:eval") {
-            addViolation(Violation(ctx!!.FullQName().symbol.line))
+            addViolation(Violation(ctx.FullQName().symbol.line))
         }
     }
 }
 
 class XdmpValueListener : AbstractXQueryParserListener() {
     override fun enterQName(ctx: XQueryParser.QNameContext) {
-        val qname = ctx?.FullQName()?.text ?: ""
+        val qname = ctx.FullQName()?.text ?: ""
 
         if (qname.toLowerCase() == "xdmp:value") {
-            addViolation(Violation(ctx!!.FullQName().symbol.line))
+            addViolation(Violation(ctx.FullQName().symbol.line))
+        }
+    }
+}
+
+class OperationsInPredicateListener : AbstractXQueryParserListener() {
+    private val functionNames = arrayOf("data", "last", "not", "exists", "xs:integer", "string", "xs:decimal", "xs:double", "xs:float", "xs:date", "xs:dateTime", "xs:time", "xs:dayTimeDuration", "xs:yearMonthDuration", "xs:duration")
+    private val expressions = arrayOf(XQueryParser.PLUS, XQueryParser.MINUS, XQueryParser.KW_DIV, XQueryParser.MOD, XQueryParser.STAR)
+
+    override fun enterPredicate(ctx: XQueryParser.PredicateContext?) {
+        super.enterPredicate(ctx)
+
+        /* Check Expresions */
+        for (expr in expressions) {
+            if (Trees.findAllTokenNodes(ctx, expr).isNotEmpty()) {
+                addViolation(Violation(ctx!!.LBRACKET().symbol.line))
+            }
+        }
+
+        /**
+         * Check for function calls
+         * In theory, we could just report *ALL* function calls, instead of the ones listed in the array above.
+         * In fact, that might be better, but currently reimplementing rules from previous edition.
+         * Unfortunately, if the user passes in fn:string() instead of just string(), this will *NOT* currently be caught.
+         **/
+        for (fn in Trees.findAllRuleNodes(ctx, XQueryParser.RULE_functionCall)) {
+            /* Fully Qualified Names */
+            for (fqn in Trees.findAllTokenNodes(fn, XQueryParser.FullQName)) {
+                if (functionNames.contains(fqn.text)) {
+                    addViolation(Violation(ctx!!.LBRACKET().symbol.line))
+                }
+            }
+
+            /* not fully qualified names */
+            for (ncn in Trees.findAllTokenNodes(fn, XQueryParser.NCName)) {
+                if (functionNames.contains(ncn.text)) {
+                    addViolation(Violation(ctx!!.LBRACKET().symbol.line))
+                }
+            }
+        }
+    }
+}
+
+class StrongTypingInFlworListener : AbstractXQueryParserListener() {
+    override fun enterLetClause(ctx: XQueryParser.LetClauseContext?) {
+        super.enterLetClause(ctx)
+
+        // allowing bypass of $_ variable name from strong typing
+        if (ctx?.letBinding?.typeDeclaration() == null && !(ctx!!.letBinding.varName().text == ("_"))) {
+            addViolation(Violation(ctx.KW_LET().symbol.line))
+        }
+    }
+
+    override fun enterForClause(ctx: XQueryParser.ForClauseContext?) {
+        super.enterForClause(ctx)
+
+        if (ctx?.forBinding()?.size ?:0 > 0)
+            for (binding in ctx!!.forBinding()) {
+                if (binding?.typeDeclaration() == null) {
+                    addViolation(Violation(ctx.KW_FOR().symbol.line))
+                }
+            }
+    }
+}
+
+class FunctionMappingListener : AbstractXQueryParserListener() {
+    override fun enterModule(ctx: XQueryParser.ModuleContext?) {
+        super.enterModule(ctx)
+
+        if (ctx!!.childCount > 0) {
+            var capable = false
+            var lineNumber = 1
+            for (child in ctx.children) {
+                when (child.payload) {
+                    is XQueryParser.VersionDeclContext -> {
+                        if ("\"1.0-ml\"" == (child.payload as XQueryParser.VersionDeclContext).version?.text) {
+                            capable = true
+                        }
+                    }
+                    is XQueryParser.MainModuleContext -> {
+                        val mmCtx = (child.payload as XQueryParser.MainModuleContext)
+                        if (capable && mmCtx.prolog() != null) {
+                            checkProlog(mmCtx.prolog(), if (lineNumber == 1) 1 else lineNumber + 1)
+                        }
+                        capable = false // reset for next main module, needs new xquery version node to set capable
+                    }
+                    is XQueryParser.LibraryModuleContext -> {
+                        val lmCtx = (child.payload as XQueryParser.LibraryModuleContext)
+                        if (capable && lmCtx.prolog() != null) {
+                            checkProlog(lmCtx.prolog(), lineNumber)
+                        }
+                    }
+                    is Token -> lineNumber = (child.payload as Token).line
+                }
+            }
+        }
+    }
+
+    private fun checkProlog(ctx: XQueryParser.PrologContext, line: Int) {
+        for(option in ctx.optionDecl()) {
+            if ("xdmp:mapping" == option.name.text) return
+        }
+        addViolation(Violation(line))
+    }
+}
+
+class XPathDescendantStepsListener : AbstractXQueryParserListener() {
+    override fun enterRelativePathExpr(ctx: XQueryParser.RelativePathExprContext?) {
+        super.enterRelativePathExpr(ctx)
+
+        if (ctx!!.DSLASH().size > 0) {
+            addViolation(Violation(ctx.start.line))
+        }
+    }
+
+    override fun enterPathExpr(ctx: XQueryParser.PathExprContext?) {
+        super.enterPathExpr(ctx)
+
+        if (ctx!!.DSLASH() != null) {
+            addViolation(Violation(ctx.start.line))
+        }
+    }
+}
+
+class XPathTextStepsListener : AbstractXQueryParserListener() {
+    override fun enterFunctionCall(ctx: XQueryParser.FunctionCallContext?) {
+        super.enterFunctionCall(ctx)
+
+        if (ctx!!.eqName().text == "text") {
+            addViolation(Violation(ctx.start.line))
         }
     }
 }
